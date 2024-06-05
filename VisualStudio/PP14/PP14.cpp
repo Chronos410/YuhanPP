ef9987c9996a48aed79c6f7a4e7fdff81c650514
Chronos410 <chronos410@naver.com>
2024-06-05 11:45:34 +0900

    [CYS] 14주차 수업 결과물 (중력 가속도)

--- VisualStudio/PP14/PP14.cpp
+++ VisualStudio/PP14/PP14.cpp
@@ -1,23 +1,27 @@
 ﻿#pragma comment(lib, "opengl32.lib")    //추가 종속성 lib 추가 하는 법
 #include <iostream>
 #include <GLFW/glfw3.h>
-#include "Land.h"
-#include "Player.h"
+#include <chrono>
 
 float playerX = -0.5f;
 float playerY = -0.5f;
 float playerX_velocity = 0.0f;
 float playerY_velocity = 0.0f;
-float playerScale = 0.05f;
+float playerSize = 0.1f;
+float playerScale = playerSize / 2.0f;
 
 float landX = 0.0f;
-float landXscale = 1.0f;
+float landXsize = 2.0f;
+float landXscale = landXsize / 2.0f;
 float landY = -0.9f;
-float landYscale = 0.1f;
+float landYsize = 0.2f;
+float landYscale = landYsize / 2.0f;
 
-float gravity = 0.00098f;
+float gravity = (9.8f * 0.1f) / (60.0f);
 
-bool isLanded = false;
+//디버그목적
+int ss;
+float lowVelocity = 0.0f;
 
 void errorCallback(int error, const char* description) {
     std::cerr << "코드" << error << description;
@@ -29,13 +33,13 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
 
-    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS && isLanded) 
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
     {
-        playerY_velocity = 0.02f;
+        playerY_velocity = playerY_velocity + 0.1f;
     }
 }
 
-void render_Land() 
+void render_Land()
 {
     glBegin(GL_QUADS);
     glColor3f(1.0f, 1.0f, 1.0f);
@@ -47,7 +51,7 @@ void render_Land()
 
     glEnd();
 }
-void render_Player() 
+void render_Player()
 {
     glBegin(GL_QUADS);
     glColor3f(1.0f, 0.0f, 0.0f);
@@ -69,7 +73,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1000, 1000, "GameEngine4", NULL, NULL);
+    window = glfwCreateWindow(1000, 1000, "Lecture14", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -81,18 +85,34 @@ int main(void)
     glfwSetErrorCallback(errorCallback);
     glfwSetKeyCallback(window, key_callback);
 
+    glfwSwapInterval(1);//프레임을 모니터 주사율에 맞춤 => 지금은 60fps
+
+    auto startTime = std::chrono::system_clock::now();
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        std::chrono::duration<double>sec = std::chrono::system_clock::now() - startTime;
+
         glfwPollEvents();
         /* Render here */
 
         glClearColor(1.0f, 0.0f, 1.0f, 1.0f); //clear 할 색
         glClear(GL_COLOR_BUFFER_BIT);
 
+        if ((playerX - playerScale) > (landX - landXscale) && (playerX - playerScale) < (landX + landXscale) || (playerX + playerScale) > (landX - landXscale) && (playerX + playerScale) < (landX + landXscale))
+        {
+            if ((playerY - playerScale) > (landY - landYscale) && (playerY - playerScale) < (landY + landYscale) || (playerY + playerScale) > (landY - landYscale) && (playerY + playerScale) < (landY + landYscale))
+            {
+                playerY_velocity = 0.0f;
+            }
+        }
+
+        playerY_velocity = playerY_velocity - gravity;
+
         playerX = playerX + playerX_velocity;
         playerY = playerY + playerY_velocity;
-        playerY_velocity = playerY_velocity - gravity;
+        //속도 적용
 
         if (playerY - playerScale < landY + landYscale)     //땅 뚫고 들어가기 방지
         {
@@ -100,24 +120,14 @@ int main(void)
             playerY_velocity = 0.0f;
         }
 
-        //render_Land();
-        //render_Player();
-        Land land1;
-        land1.RenderLand(landX, landY, landXscale, landYscale);
-        Player(playerX, playerY, playerScale, playerX_velocity, playerY_velocity);
-        
+        render_Land();
+        render_Player();
+        //연산 끝나고 렌더
 
-        if ((playerX - playerScale) > (landX - landXscale) && (playerX - playerScale) < (landX + landXscale) || (playerX + playerScale) > (landX - landXscale) && (playerX + playerScale) < (landX + landXscale))
+        if (sec.count() > 2 && sec.count() < 3) 
         {
-            if ((playerY - playerScale) > (landY - landYscale) && (playerY - playerScale) < (landY + landYscale) || (playerY + playerScale) > (landY - landYscale) && (playerY + playerScale) < (landY + landYscale))
-            {
-                isLanded = true;
-                playerY_velocity = 0.0f;
-            }
-            else
-            {
-                isLanded = false;
-            }
+            std::cout << ss << std::endl;
+            ss++;
         }
 
         /* Swap front and back buffers */
